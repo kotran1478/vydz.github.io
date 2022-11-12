7ca678cca386bba4b46ebe2032611e23bfab8b27
kotran1478 <103329391+kotran1478@users.noreply.github.com>
2022-11-12 15:44:39 +0700

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,125 @@
+<style>
+    .mn {
+    border-bottom: solid 4px #dfdddd59;
+    backdrop-filter: blur(10px);
+    background: rgb(183 154 154 / 32%);
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+}
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+user agent stylesheet
+h3 {
+    display: block;
+    font-size: 1.17em;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    font-weight: bold;
+}
+.mn ul {
+    display: inline-flex;
+    list-style: none;
+    color: antiquewhite;
+}
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+user agent stylesheet
+ul {
+    display: block;
+    list-style-type: disc;
+    margin-block-start: 1em;
+    margin-block-end: 1em;
+    margin-inline-start: 0px;
+    margin-inline-end: 0px;
+    padding-inline-start: 40px;
+}
+.mn ul li {
+    width: 200px;
+    margin: 15px;
+    padding: 10px;
+    
+    border-radius: 4px;
+    transform: skewX(-10deg);
+    transition: all 0.75s;
+    text-shadow: 0 0 12px #2228efd1;
+    box-shadow: 0 0 10px black;
+    background: linear-gradient(45deg, #05dfd9, #98a9b9,#49aa1b);
+}
+.mn ul li:hover{
+    background-position:right;
+    width: 200px;
+    margin: 15px;
+    padding: 10px;
+    
+    border-radius: 4px;
+    transform: skewX(10deg);
+    transition: all 0.75s;
+    text-shadow: 0 0 12px #2228efd1;
+    box-shadow: 0 0 10px black;
+}
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+user agent stylesheet
+li {
+    display: list-item;
+    text-align: -webkit-match-parent;
+}
+.mn ul li a {
+    text-decoration: none;
+    color: antiquewhite;
+}
+* {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+user agent stylesheet
+a:-webkit-any-link {
+    color: -webkit-link;
+    cursor: pointer;
+    text-decoration: underline;
+}
+    </style>
+
+<body align="center" style="background: url('https://wallpaperaccess.com/full/201088.jpg') 0 / cover fixed;
+background-repeat: no-repeat;">
+    <div>
+        <h3 div="" class="mn">
+            <ul>
+            <li><a href="index.html" target="_self" tittle="Click to view information">
+                <b>Main Page</b>
+            </a></li>
+            
+            <li><a href="page2.html" target="_self" tittle="Click to view information">
+                <b>Events &amp; Festivals</b>
+            </a></li>
+
+            <li><a href="page3.html" target="_self" tittle="Click to view information">
+                <b>Scenic Spots</b>
+            </a></li>
+
+            <li class="active"><a href="page4.html" target="_self" tittle="Click to view information">
+                <b>Cuisine</b>
+            </a></li>
+
+            <li><a href="page5.html" target="_self" tittle="Click to view information">
+                <b>More</b>
+            </a></li>
+
+            </ul>
+        </h3>
+    </div>
+    <br><br><br><br><br><br><br><br>
