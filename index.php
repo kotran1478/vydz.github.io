60cd651030538241e2c66259b77f7a53a545a434
kotran1478 <103329391+kotran1478@users.noreply.github.com>
2022-11-12 15:44:08 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,88 @@
+<?
+require_once("nav.php");
+?>
+   
+    <title>Cuisine</title>
+    
+    <div style="
+    margin-bottom: 20px; border-bottom: solid 4px #dfdddd59;
+    backdrop-filter: blur(10px);
+    background: rgb(183 154 154 / 32%);display: inline-flex;padding: 50px;
+    width: 70pc;border-radius: 16px;
+">
+        <div class="text-center ">dâdwadadwd
+    
+    <h1>
+        Cuisine
+    </h1><br><br><br><br><br><br>
+    <div id="para1">
+        <p>
+            </p><h3><b>Pho</b></h3>
+        <p></p>
+    </div><br>
+    <div id="para1-5">
+        <p>
+            With its spectacularly delightful broth, Pho Vietnam, also known as Vietnamese noodle soup, is a highlight of Vietnamese cuisine. One of the most well-liked street foods in nearly all of Vietnam's major cities, including Hanoi, Halong, Nam Dinh, and Ho Chi Minh City, is pho. Pho is currently the only meal that can truly capture the spirit of Vietnamese cuisine.
+        </p>
+    </div><br>
+    <div id="pho">
+        <img src="picture/pho.jpg" alt="" width="800">
+    </div><br><br><br>
+    <div id="para2">
+        <p>
+            </p><h3><b>Banh Mi</b></h3>   
+        <p></p>
+    </div><br>
+    <div id="para2-5">
+        <p>
+            In essence, it is a filled crusty baguette. The ingredients might range from pieces of meatballs in a tomato sauce to pickled local vegetables and roast pork. The Vietnamese have experimented with and refined a number of various variations rather than having a set of outlined Banh Mi principles. Every neighborhood Banh Mi street chef is a master of their particular recipe. Absolutely delicious, ranked among the tastiest sandwiches in the entire world.
+        </p>
+    </div>
+    <div id="banhmi">
+        <img src="picture/banh-mi.jpg" alt="" width="800">
+    </div><br><br><br>
+    <div id="para3">
+        <p>
+            </p><h3><b>Vietnamese Spring Rolls</b></h3>  
+        <p></p>
+    </div><br>
+    <div id="para3-5">
+        <p>
+            Goi cuon, or fresh spring rolls in Vietnamese, are traditionally made with lettuce, herbs, pork, and shrimp. They make a stunning visual statement and are delicious as a light dinner or refreshing appetizer. Vietnamese spring rolls can be divided into two primary categories: fresh rolls and fried rolls. Vietnamese people refer to freshly made spring rolls (or summer rolls, rice paper rolls) as 'cha gio'. A variety of fillings are encased in rice paper, offering guests a variety of tastes and textures.
+        </p>
+    </div>
+    <div id="goicuon">
+        <img src="picture/goicuon.jpg" alt="" width="800">
+    </div><br>
+    <div id="chagio">
+        <img src="picture/chagio.jpg" alt="" width="800">
+    </div><br><br><br>
+    <div id="para4">
+        <p>
+            </p><h3><b>Com Tam</b></h3> 
+        <p></p>
+    </div><br>
+    <div id="para4-5">
+        <p>
+            Broken rice, or "Com Tam," is a delicacy of southern Vietnam and one of the region's most beloved breakfast fare. These days, it can be found abroad or in the centre and north of Vietnam. In addition to grilled pork, thinly shredded pork combined with cooked and thinly shredded pork skin, steamed egg, sunny side up egg, fresh cucumber, tomato, and some pickled vegetables are typically included in a Com Tam meal. The flavor of "com tam" is mostly influenced by seasoned fish sauce, another essential ingredient. You can find cuisine in Saigon anytime, anywhere, from roadside stalls to opulent restaurants. It has come to symbolize Saigon culture.
+        </p>
+    </div>
+    <div id="comtam">
+        <img src="picture/com-tam.jpg" alt="" width="800">
+    </div><br><br><br>
+    <div id="para5">
+        <p>
+            </p><h3><b>Bun Bo Hue</b></h3>   
+        <p></p>
+    </div><br>
+    <div id="para5-5">
+        <p>
+            The charming, old city of Hue in Central Vietnam, known for its Royal Cuisine, is where bun bo Hue first appeared. It is a beef noodle dish similar to the more well-known Pho Bo, but it is distinctively different because of its past and origins. It is a noodle soup with a hearty, spicy broth, pieces of beef or pork, and, like so many Vietnamese meals, leaves and herbs on top. It is a dinner rich in contrasting flavors and textures. The flavor and ingredients present in traditional Vietnamese dishes are generally influenced by their geographic origins and environment, but pho bo, unlike bun bo Hue, has a French influence.
+        </p>
+    </div>
+    <div id="bunbohue">
+        <img src="picture/bunbohue.jpg" alt="" width="800">
+    </div><br><br><br>
+
+
+<div id="D6E7DCF2-0427-78AF-47E8-48D3F01FA6A8"></div></body></html>
